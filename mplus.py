cc384a7b02c3736f661a9fc8364760fe66178b18
mahirmingsar-droid <mahirmingsar@gmail.com>
2026-03-28 22:22:05 +0300

    Create mplus.py
    My new code language M+

---
+++ mplus.py
@@ -0,0 +1,7 @@
+mplus.py kod = input("M+ kodunu gir: ")
+
+if "Mouse" in kod and "Mahro" in kod:
+    print("if (isMouseClickedOn(Mahro)) {")
+    print("  for(int i=0;i<2;i++){ Mahro.jump(); }")
+    print("}") 
+If mouse clicks Mahro(Character Name),Character will jump.
